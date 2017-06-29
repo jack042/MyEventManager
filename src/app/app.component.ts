380846e4320b8cd2655d31d0ca8ebdcc5c3ab4c2
jack042 <riyo.putra@gmail.com>
2017-06-29 09:17:54 +0700

    Initial
    initial version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -8,15 +10,37 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any;
+
+
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyCyKzqdXO-wr3lgxPtf5CXG_wImnUMjqMM",
+      authDomain: "myeventmanager-b6085.firebaseapp.com",
+      databaseURL: "https://myeventmanager-b6085.firebaseio.com",
+      projectId: "myeventmanager-b6085",
+      storageBucket: "",
+      messagingSenderId: "940504852815"
+    });
+
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if(!user) {
+        this.rootPage = 'login';
+        unsubscribe();
+      } else {
+        this.rootPage = HomePage;
+        unsubscribe();
+      }
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
   }
 }
-
